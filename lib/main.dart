1c117b6c5305c00b9758de33d13df709327c1401
Your Name <ahmadmahmood296@gmail.com>
2023-04-02 00:57:03 +0500

    Splash Screen Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import './theme/theme_data.dart';
 import 'View/onboarding_page.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.removeAfter(initialization);
   runApp(const MyApp());
 }
-
+Future initialization(BuildContext? context) async {
+  await Future.delayed(const Duration(seconds: 2));
+}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
